938c8bbf2f3f5cef125620edceece76378e35079
ttngu <tammyn137@gmail.com>
2020-11-04 22:05:20 -0800

    added test question, shuffle and question vars

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-var startButton = document.getElementById("start-btn")
-var questionContainerElement= document.getElementById("question-container")
+var startButton = document.getElementById("start-btn");
+// var questionContainerEl= document.getElementById("question-container");
+
+var shuffledQuestions, currentQuestionIndex;
+
 // why doesn't this work??, it will not console log says "TypeError"
-startButton.addEventListener('click', startGame())
+startButton.addEventListener('click', startGame);
 
 function startGame(){
-    console.log('Started')
-    startButton.classList.add("hide")
+    console.log("Started");
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove("hide");
+    setNextQuestion();
 }
 
 function setNextQuestion() {
@@ -15,3 +22,15 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+var questions = [
+    { question: "What Pokemon does Pikachu evolve into?",
+        answers: [
+            {text: "Raichu", correct: true},
+            {text: "Electivire", correct: false},
+            {text: "Meowstic", correct: false},
+            {text: "Jolteon", correct: false},
+        ]
+    }
+    
+]
